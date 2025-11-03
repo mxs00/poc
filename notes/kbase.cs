48c37edf7b5861b15dda3ef5aaf345e90d283d66
mxs00 <39170123+mxs00@users.noreply.github.com>
2025-11-03 17:27:06 +0300

    Update kbase.cs

--- notes/kbase.cs
+++ notes/kbase.cs
@@ -3,6 +3,9 @@
 //ref https://www.index.dev/blog/how-to-set-global-variables-python
 //ref: https://dev.to/igorbenav/fastapi-mistakes-that-kill-your-performance-2b8k
 
+pip install uvloop 
+pip install httptools
+
 
 //---------------------------------------------------
 # Good - Dataclass for internal processing
